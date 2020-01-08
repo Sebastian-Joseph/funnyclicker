d808bd936a5d832a6523f2162a0dc95c469c8e58
Keniran <naani.navis@gmail.com>
2020-01-08 11:36:26 -0500

    implemented click upgrade function

--- juice.js
+++ juice.js
@@ -5,10 +5,11 @@ let clickBonus = 0;
 let clickLevelCost = 500;
 let autoTonyJuice = 0;
 
-const COST_INCREASE = 1.7;
+const COST_INCREASE = 1.6;
+const CLICK_COST_INCREASE = 5;
 
 let juicer = {quantity: 0, juiceYield: 1, cost: 30};
-let farm = {quantity: 0, juiceYield: 12, cost: 1500};
+let farm = {quantity: 0, juiceYield: 8, cost: 1500};
 let factory = {quantity: 0, juiceYield: 0, cost: 0};
 let office = {quantity: 0, juiceYield: 0, cost: 0};
 let elon = {quantity: 0, juiceYield: 0, cost: 0};
@@ -18,11 +19,26 @@ let ai = {quantity: 0, juiceYield: 0, cost: 0};
 
 window.onload = function() {
     document.getElementById("bottle").onclick = clickFunction;
+    document.getElementById("click-bonus-button").onclick = clickLevelUp;
     document.getElementById("juicer-button").onclick = buyJuicer;
     document.getElementById("farm-button").onclick = buyFarm;
 
     autoProduction();
     updateVariables();
+};
+
+const updateVariables = function() {
+    clickBonus = Math.round(0.01 * autoTonyJuice * clickLevel * 8);
+    tonyJuicePerClick = 1 + clickBonus;
+    autoTonyJuice = (juicer.quantity * juicer.juiceYield) + (farm.quantity * farm.juiceYield) + (factory.quantity * factory.juiceYield) + (office.quantity * office.juiceYield)
+     + (elon.quantity * elon.juiceYield) + (kingdom.quantity * kingdom.juiceYield) + (planet.quantity * planet.juiceYield) + (ai.quantity * ai.juiceYield);
+
+    const x = document.getElementById("tj-per-sec");
+    const y = document.getElementById("tj-per-click");
+    x.innerHTML = (autoTonyJuice * 8).toLocaleString("en");
+    y.innerHTML = tonyJuicePerClick.toLocaleString("en");;
+
+    setTimeout(updateVariables, 10);
 };
 
 const clickFunction = function() {
@@ -32,17 +48,18 @@ const clickFunction = function() {
     x.innerHTML = tonyJuice.toLocaleString("en");
 };
 
-const updateVariables = function() {
-    clickBonus = Math.ceil(0.1 * autoTonyJuice * clickLevel);
-    tonyJuicePerClick += clickBonus;
-    autoTonyJuice = (juicer.quantity * juicer.juiceYield) + (farm.quantity * farm.juiceYield) + (factory.quantity * factory.juiceYield) + (office.quantity * office.juiceYield)
-     + (elon.quantity * elon.juiceYield) + (kingdom.quantity * kingdom.juiceYield) + (planet.quantity * planet.juiceYield) + (ai.quantity * ai.juiceYield);
+const clickLevelUp = function() {
+    if (tonyJuice >= clickLevelCost) {
+        tonyJuice -= clickLevelCost;
+        clickLevel++;
+        clickLevelCost *= CLICK_COST_INCREASE;
 
-    const x = document.getElementById("tj-per-sec");
-    x.innerHTML = (autoTonyJuice * 8).toLocaleString("en");
-
-    setTimeout(updateVariables, 10);
-};
+        const x = document.getElementById("clicking-bonus");
+        const y = document.getElementById("click-upgrade-cost");
+        x.innerHTML = clickLevel.toLocaleString("en");
+        y.innerHTML = clickLevelCost.toLocaleString("en");
+    }
+}
 
 const autoProduction = function() {
     tonyJuice += autoTonyJuice;
